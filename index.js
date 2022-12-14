207025a722f8cdad9bb4907af49b0065308369c7
farhanacsebd <farhana.cse.bd@gmail.com>
2022-12-14 22:01:42 +0600

    added js

---
+++ index.js
@@ -0,0 +1,13 @@
+const bgImg = document.getElementById('bg-image');
+
+window.addEventListener('scroll',()=>{
+    updateImage();
+})
+
+function updateImage(){
+    bgImg.style.opacity = 1 - +window.pageYOffset/550+'';
+    bgImg.style.top = +window.pageYOffset + 'px';
+    bgImg.style.backgroundPositionY = -+window.pageYOffset/2 + 'px';
+    // console.log(1 - window.pageYOffset/900);
+   
+}
